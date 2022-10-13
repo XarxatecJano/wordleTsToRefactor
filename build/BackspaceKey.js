d5d822cbdb7a6d0c00ffff59520cee682e51ec29
Semi <Juan.Esteban@xarxatecactiva.com>
2022-10-13 19:34:04 +0200

    fix: backspace  key

--- build/BackspaceKey.js
+++ build/BackspaceKey.js
@@ -4,6 +4,7 @@ export class BackspaceKey extends Key {
         if (super.game.currentPosition > 0) {
             super.game.currentPosition -= 1;
             super.game.userInterface.deleteLetter(this.game.turn, this.game.currentPosition);
+            super.game.currentWord = super.game.currentWord.slice(0, -1);
         }
     }
 }
